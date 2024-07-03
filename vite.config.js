4dcdc803f51781379ee7f02cb0d6c3e3aeeabf83
hanna lam <63362052+hannalam@users.noreply.github.com>
2024-07-04 00:15:10 +0200

    update again

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./hannalam.github.io",
+  base: "/hannalam.github.io",
 })
